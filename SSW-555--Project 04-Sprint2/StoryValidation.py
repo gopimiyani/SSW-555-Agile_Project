e1cceff85397250f8d872c1273a7934ffd2cefd5
dhaval-dongre <47120864+dhaval-dongre@users.noreply.github.com>
2019-03-25 14:27:45 -0400

    Update StoryValidation.py

--- SSW-555--Project 04-Sprint2/StoryValidation.py
+++ SSW-555--Project 04-Sprint2/StoryValidation.py
@@ -14,12 +14,14 @@ def story_validation(individuals, families):
     print('Validating User Stories..')
     print('\n========================================================= Reporting Errors ================================================================\n')
 
+    returnFlag_US01 = UserStories.US01_dates_before_currentDate(individuals, families)
     returnFlag_US02 = UserStories.US02_birth_before_marriage(individuals,families)
     returnFlag_US03 = UserStories.US03_birth_before_death(individuals)
     returnFlag_US04 = UserStories.US04_marriage_before_divorce(families)
     returnFlag_US05 = UserStories.US05_marriage_before_death(individuals, families)
     returnFlag_US06 = UserStories.US06_Divorce_before_death(individuals, families)
     returnFlag_US07 = UserStories.US07(individuals)
+    returnFlag_US08 = UserStories.US08_childbirth_after_marriage(individuals, families)
     returnFlag_US12 = UserStories.US12_parents_not_too_old(individuals, families)
     returnFlag_US13 = UserStories.US13_siblings_spacing(individuals, families)
     
