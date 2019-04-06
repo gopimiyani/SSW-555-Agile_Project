aae0befa22357d8c95311846a6cd3bf664b12be0
Krutarth Trivedi <47464196+Krutarth29@users.noreply.github.com>
2019-04-05 20:18:38 -0400

    Update UnitTest.py

--- SSW-555--Project 04-Sprint2/UnitTest.py
+++ SSW-555--Project 04-Sprint2/UnitTest.py
@@ -17,7 +17,8 @@ cur_path = os.path.dirname(__file__)
 
 passFile = "SSW-555-Agile-Project-01_UserStories.ged"
 #failFile = "SSW-555-Agile-Project-01_UserStories_Err.ged"
-failFile="My-Family_EditedByGopi_Errors.ged"
+#failFile="My-Family_EditedByGopi_Errors.ged"
+failFile= "Family-sprin2-error.ged"
 
 class test_UserStories(unittest.TestCase):
     
@@ -41,14 +42,14 @@ class test_UserStories(unittest.TestCase):
 
     def test_US03(self):
         print('\nTesting US_03...')
-        individuals, families = GedcomParser.gedcomParser(passFile)
+        individuals = GedcomParser.gedcomParser(passFile)
         self.assertEqual(UserStories.US03_birth_before_death(individuals), True)
-        individuals, families = GedcomParser.gedcomParser(failFile)
+        individuals = GedcomParser.gedcomParser(failFile)
         self.assertEqual(UserStories.US03_birth_before_death(individuals), False)
      # ------------------------------- TESTING US_04 -------------------------------------
     def test_US04(self):
         print('\nTesting US_04...')
-        individuals, families = GedcomParser.gedcomParser(failFile)
+        families = GedcomParser.gedcomParser(failFile)
         #self.assertTrue(US04_marriage_before_divorce(families))
         self.assertNotEqual(UserStories.US04_marriage_before_divorce(families), True)
 
@@ -73,9 +74,9 @@ class test_UserStories(unittest.TestCase):
 # --------------------------- TESTING US_07 -------------------------------------------
     def test_US07(self):
         print('\nTesting US_07...')
-        individuals, families = GedcomParser.gedcomParser(passFile)
+        individuals = GedcomParser.gedcomParser(passFile)
         self.assertTrue(UserStories.US07(individuals))
-        individuals, families = GedcomParser.gedcomParser(failFile)
+        individuals = GedcomParser.gedcomParser(failFile)
         self.assertFalse(UserStories.US07(individuals))
     
 
@@ -87,25 +88,30 @@ class test_UserStories(unittest.TestCase):
         individuals, families = GedcomParser.gedcomParser(failFile)
         self.assertEqual(UserStories.US08_childbirth_after_marriage(individuals,families), False)
         
-# --------------------------- TESTING US_09 -------------------------------------------
+# ------------------------------- TESTING US_09 -------------------------------------
+
+    def test_US09(self):
+        print("\n======================Performing Unit test on User Stories ================\n")
+        print('TESTING US_09...')
+        individuals, families = GedcomParser.gedcomParser(passFile)
+        self.assertEqual(UserStories.US09_birth_before_death_of_parents(individuals,families), True)
+        individuals, families = GedcomParser.gedcomParser(failFile)
+        self.assertEqual(UserStories.US09_birth_before_death_of_parents(individuals,families), False)
         
-        
-# --------------------------- TESTING US_10 -------------------------------------------
+# --------------------------- TESTING US_10 ------------------------------------------
     def test_US10(self):
         print("\n======================Performing Unit test on User Stories ================\n")
         print('TESTING US_10...')
         individuals, families = GedcomParser.gedcomParser(failFile)
         self.assertEqual(UserStories.US10_marriage_age_14(individuals,families), True)    
-    
 # --------------------------- TESTING US_11 -------------------------------------------
+    
     def test_US11(self):
         print("\n======================Performing Unit test on User Stories ================\n")
         print('TESTING US_11...')
         individuals, families = GedcomParser.gedcomParser(failFile)
         self.assertEqual(UserStories.US11_no_bigamy(individuals,families), True)    
         
-       
-       
 # --------------------------- TESTING US_12 -------------------------------------------NEW AFTER REFACTORING
         
     def test_US12_NEW(self):
@@ -114,8 +120,6 @@ class test_UserStories(unittest.TestCase):
         self.assertTrue(UserStories.US12_parents_not_too_old(individuals,families))
         individuals, families = GedcomParser.gedcomParser(failFile)
         self.assertFalse(UserStories.US12_parents_not_too_old(individuals,families))
-        
-  
 
 # --------------------------- TESTING US_13 -------------------------------------------NEW AFTER REFACTORING
         
@@ -127,7 +131,7 @@ class test_UserStories(unittest.TestCase):
         self.assertFalse(UserStories.US13_siblings_spacing(individuals,families))
         
 # --------------------------- TESTING US_14 -------------------------------------------
-def test_US14(self):
+    def test_US14(self):
         print('TESTING US_14...')
         individuals, families = GedcomParser.gedcomParser(passFile)
         self.assertTrue(UserStories.US14_Multiple_births(individuals, families))
@@ -135,15 +139,21 @@ def test_US14(self):
         self.assertFalse(UserStories.US14_Multiple_births(individuals, families))
 
 # --------------------------- TESTING US_15 -------------------------------------------
-def test_US15(self):
-    print('TESTING US_15...')
-    individuals, families = GedcomParser.gedcomParser(passFile)
-    self.assertTrue(UserStories.US15_Fewer_than_15_siblings(families))
-    individuals, families = GedcomParser.gedcomParser(failFile)
-    self.assertFalse(UserStories.US15_Fewer_than_15_siblings(families))
-        
-        
-# --------------------------- TESTING US_16 -------------------------------------------
+    def test_US15(self):
+        print('TESTING US_15...')
+        families = GedcomParser.gedcomParser(passFile)
+        self.assertTrue(UserStories.US15_Fewer_than_15_siblings(families))
+        families = GedcomParser.gedcomParser(failFile)
+        self.assertFalse(UserStories.US15_Fewer_than_15_siblings(families))
+                
+# ------------------------------- TESTING US_31 -------------------------------------
+    def test_US31(self):
+        print("\n======================Performing Unit test on User Stories ================\n")
+        print('TESTING US_31...')
+        individuals = GedcomParser.gedcomParser(passFile)
+        self.assertEqual(UserStories.US31_List_living_single(individuals), True)
+        individuals = GedcomParser.gedcomParser(failFile)
+        self.assertEqual(UserStories.US31_List_living_single(individuals), False)     
         
 
 
@@ -152,12 +162,17 @@ def test_US15(self):
 ##############################       MAIN METHOD START    ########################################
 
 #FILENAME = 'SSW-555-Agile-Project-01_UserStories_Err.ged'
-FILENAME='My-Family_EditedByGopi_Errors.ged'
+#FILENAME='My-Family_EditedByGopi_Errors.ged'
+FILENAME='Family-sprin2-error.ged'
 if __name__ == '__main__':
    
     individuals, families = GedcomParser.gedcomParser(FILENAME)
-    
     unittest.main()
 
 
 ##############################       MAIN METHOD END    #########################################
+
+
+
+
+
