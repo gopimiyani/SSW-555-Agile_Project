0bde67f686f5b42271541f25306925553229fd5c
Gopi Miyani <43049866+gopimiyani@users.noreply.github.com>
2019-04-22 21:40:34 -0400

    Update StoryValidation.py

--- SSW-555--Project 04-Sprint4/StoryValidation.py
+++ SSW-555--Project 04-Sprint4/StoryValidation.py
@@ -27,7 +27,7 @@ def story_validation(individuals, families):
     returnFlag_US08 = UserStories.US08_childbirth_after_marriage(individuals, families)
     returnFlag_US09 = UserStories.US09_birth_before_death_of_parents(individuals, families)
     returnFlag_US10 = UserStories.US10_marriage_age_14(individuals, families)
-    #returnFlag_US11 = UserStories.US11_no_bigamy(individuals, families)
+    returnFlag_US11 = UserStories.US11_no_bigamy(individuals, families)
     returnFlag_US12 = UserStories.US12_parents_not_too_old(individuals, families)
     returnFlag_US13 = UserStories.US13_siblings_spacing(individuals, families)
     returnFlag_US14 = UserStories.US14_Multiple_births(individuals, families)
@@ -55,7 +55,7 @@ def story_validation(individuals, families):
    
     returnFlags={'US01':returnFlag_US01,'US02':returnFlag_US02,'US03':returnFlag_US03,'US04':returnFlag_US04,
                  'US05':returnFlag_US05,'US06':returnFlag_US06,'US07':returnFlag_US07,'US08':returnFlag_US08,
-                 'US09':returnFlag_US09,'US10':returnFlag_US10,"""'US11':returnFlag_US11,"""'US12':returnFlag_US12,
+                 'US09':returnFlag_US09,'US10':returnFlag_US10,'US11':returnFlag_US11,'US12':returnFlag_US12,
                  'US13':returnFlag_US13,'US14':returnFlag_US14,'US15':returnFlag_US15,'US16':returnFlag_US16,
                  'US18':returnFlag_US18,'US19':returnFlag_US19,'US20':returnFlag_US20,'US21':returnFlag_US21,
                  'US22':returnFlag_US22,'US23':returnFlag_US23,'US24':returnFlag_US24,'US25':returnFlag_US25,
